f289de551ecc43e940bd419090c066666c8c5fef
maneeshjohn <maneeshjohno@gmail.com>
2024-09-16 17:24:18 +0530

    added media enum and catalog struch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+#[derive(Debug)]
+enum Media {
+    Book { title: String, author: String },
+    Movie { title: String, director: String },
+    AudioBook { title: String }
+}
+
+#[derive(Debug)]
+struct Catalog {
+    items: Vec<Media>
+}
+
+impl Catalog {
+    fn new() -> Self {
+        return Catalog {
+            items: vec![]
+        };
+    }
+
+    fn add(&mut self, media: Media) {
+        self.items.push(media);
+    }
+
+    fn get(&self, idx: usize) -> Option<&Media> {
+        if idx < self.items.len() {
+            return Some(&self.items[idx]);
+        } else {
+            return None;
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let movie = Media::Movie { title: String::from("Movie"), director: String::from("Director") };
+    let mut catalog = Catalog::new();
+    catalog.add(movie);
+
+    match catalog.get(0) {
+        Some(media) => println!("C: {:#?}", media),
+        None => println!("Not found")
+    }
 }
